5117c7a7d4dc59e3b44c331579b30672f5c0bcb2
Aaron Ballard <ans.ballard@gmail.com>
2024-08-27 14:29:27 -0600

    first pass

--- src/datetime/relative_time.ts
+++ src/datetime/relative_time.ts
@@ -1,24 +1,19 @@
-//REF: https://github.com/home-assistant/frontend/blob/dev/src/common/datetime/relative_time.ts
+import memoizeOne from "memoize-one";
+import { FrontendLocaleData } from "../data/translation";
+import { selectUnit } from "../util/select-unit";
 
-import { selectUnit } from "@formatjs/intl-utils";
-import { FrontendLocaleData } from "../types";
-
- const formatRelTimeMem =
+const formatRelTimeMem = memoizeOne(
   (locale: FrontendLocaleData) =>
-    new Intl.RelativeTimeFormat(locale.language, { numeric: "auto" });
+    new Intl.RelativeTimeFormat(locale.language, { numeric: "auto" })
+);
 
-/**
- * Calculate a string representing a date object as relative time from now.
- *
- * Example output: 5 minutes ago, in 3 days.
- */
- export const relativeTime = (
+export const relativeTime = (
   from: Date,
   locale: FrontendLocaleData,
   to?: Date,
   includeTense = true
 ): string => {
-  const diff = selectUnit(from, to);
+  const diff = selectUnit(from, to, locale);
   if (includeTense) {
     return formatRelTimeMem(locale).format(diff.value, diff.unit);
   }
